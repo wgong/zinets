49b51300fa5a07fc2c0c0dc44bd630ca251e375a
Gong, Wen <wen.g.gong@gmail.com>
2025-05-07 07:33:04 -0400

    update

--- app/zadmin/000_run_app.bat
+++ app/zadmin/000_run_app.bat
@@ -1,5 +1,5 @@
 rem streamlit run zi_admin.py
-conda activate zinet
-cd C:\Users\p2p2l\projects\wgong\zistory\zinets\app\zadmin
+rem conda activate zinet
+rem cd C:\Users\p2p2l\projects\wgong\zistory\zinets\app\zadmin
 streamlit run ZiNets.py
 
